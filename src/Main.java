5d66f46540fe0b71b5c621fbe64aa39c7a5438c7
ninaf <gogatsun@gmail.com>
2023-09-24 08:36:38 +0300

    Fifth commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Game: Stone Scissors Paper");
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("\nGame: %d", i);
+            Game.run();
+        }
     }
 }
